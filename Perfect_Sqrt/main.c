87bc23086542a7b0ad6340ee4309bf4a9dadc6ea
BuildTools <unconfigured@null.spigotmc.org>
2021-10-27 12:59:19 +0100

    Initial Commit

---
+++ Perfect_Sqrt/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Perfect_Sqrt
+//
+//  Created by Diakite on 27/10/2021.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
